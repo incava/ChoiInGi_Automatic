e005ef99577a5947b5d5ad9684f0827ee07c7b3f
incava <ingi1118@naver.com>
2023-02-11 23:52:28 +0900

    add : 멀티쓰레드 기능 & mac버전에서 자동화, requriements freeze, 자동화 실행 및 종료 가능, filter기능 활성화

--- coupang.py
+++ coupang.py
@@ -1,5 +1,3 @@
-import sys
-import threading
 import time
 import subprocess
 from selenium import webdriver
@@ -14,27 +12,45 @@ import pandas as pd
 
 class Pasing:
     # 필터값을 설정 하기 위해 생성자를 받도록함.
-    def __init__(self, url, search_name, min_price=0, max_price=None, contain_names=None):
-        if contain_names is None:
-            contain_names = []
+    def __init__(self, url, search_name, min_price, max_price, contain_names):
+        self.driver = None
+        self.url = url
+        self.search_name = search_name
+        self.min_price = min_price
+        self.max_price = max_price
+        self.contain_names = contain_names
+        self.nameAry = []  # 상품 이름 리스트
+        self.priceAry = []  # 상품 가격 리스트
+        self.threadStop = False
+
+    def set_path(self):
+        option = webdriver.ChromeOptions()
+        option.add_argument('--no-sandbox')
+        option.add_argument("disable-gpu")
+        option.add_argument("--lang=ko_KR")
+        option.add_argument(
+            'user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36')
+        option.add_argument('--disable-blink-features=AutomationControlled')
+        option.add_argument("--disable-extensions")
+        option.add_experimental_option('useAutomationExtension', False)
+        option.add_experimental_option("excludeSwitches", ["enable-automation"])
+        # 크롬 자동 드라이버 설치 및 버전 맞춤.
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
+        self.driver.get(self.url)
+
+    def set_member(self, url, search_name, min_price, max_price, contain_names):
         self.contain_names = contain_names
         self.url = url
         self.search_name = search_name
         self.min_price = min_price
         self.max_price = max_price
-        self.nameAry = []  # 상품 이름 리스트
-        self.priceAry = []  # 상품 가격 리스트
-        subprocess.Popen(
-            r'C:\Program Files\Google\Chrome\Application\chrome.exe --remote-debugging-port=9222 --user-data-dir="C:\chromeCookie"')
-        # Navigate to the CoupangLogin homepage
-        option = Options()
-        option.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-        # 크롬 자동 드라이버 설치 및 버전 맞춤.
-        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=option)
 
     def ary_clear(self):
         self.nameAry.clear()
         self.priceAry.clear()
+
+    def thread_control(self, true_false):
+        self.threadStop = true_false
 
     def set_instance(self, url, search_name, min_price, max_price, contain_names):
         self.contain_names = contain_names
@@ -63,8 +79,9 @@ class Pasing:
         # 필더가 있다면 확인
 
     def auto_start(self):
+        # 예기치 못한 상황이라면 error+msg, 성공적이면 success, 실패라면 fail, 종료를 원했다면 exit
+        self.set_path()
         self.ary_clear()
-        self.driver.get(self.url)
         self.driver.implicitly_wait(10)
         # Find the search box element and enter a query
         search_box = self.driver.find_element(By.NAME, "q")
@@ -76,6 +93,9 @@ class Pasing:
             datas = self.driver.find_elements(By.CSS_SELECTOR, ".search-product")
             cnt = 0
             for item in datas:
+                if self.threadStop:  # 사용자가 원하는 종료
+                    self.driver.quit()
+                    return 'exit'
                 try:  # 예외 처리 구문으로
                     name = item.find_element(By.XPATH, "a/dl/dd/div/div[2]").text
                     price = item.find_element(By.XPATH, "a/dl/dd/div/div[3]/div/div[1]/em/strong").text
@@ -89,6 +109,7 @@ class Pasing:
                     print(f"NoSuchElementError : {e}")
                 except Exception as e:  # 모든 에러를 잡기 위해 예외.
                     print(f"Anonymous Error : {e}")
+                    return 'error: ' + str(e)  # 예기치 못한 상황에 대한 종료
                 finally:
                     cnt += 1
                     if cnt % 3 == 1:  # 무조건 3번마다 포인팅하면서 스크롤이 필요하다면 스크롤.
@@ -100,13 +121,20 @@ class Pasing:
             try:  # 마지막 페이지에 대한 예외 처리
                 next_page_link = self.driver.find_element(By.CSS_SELECTOR, '.btn-next')
                 next_page_link.click()
+            except NoSuchElementException as e:  # 만약 next_page_link가 더이상 없다면
+                return 'success'
             except Exception as e:  # 여러 예외 처리
-                print(f"Exception :{e}")
                 self.driver.quit()
-                break
-        data = {"product": self.nameAry, "price": self.priceAry}  # 딕셔너리로 저장
-        df = pd.DataFrame(data)
-        df.to_csv("test.csv", encoding="utf-8-sig")  # csv로 저장.
-        self.driver.quit()
+                return 'error: ' + str(e)
+
+    def ary_to_csv(self):
+        try:
+            data = {"product": self.nameAry, "price": self.priceAry}  # 딕셔너리로 저장
+            df = pd.DataFrame(data)
+            df.to_csv("test.csv", encoding="utf-8-sig")  # csv로 저장.
+            self.driver.quit()
+            return 'success'
+        except Exception as e:
+            return 'error: ' + str(e)
 
 
--- coupang_test.py
+++ coupang_test.py
@@ -1,10 +1,99 @@
 import sys
+import tkinter as tk
 from coupang import Pasing
-
-url = "https://www.coupang.com"
-search_name = "아이패드"
-filter_name = ["프로"]
-auto = Pasing(url, search_name, 1000000, sys.maxsize, filter_name)
-auto.auto_start()
+import threading
 
 
+class Macro:
+    def __init__(self):
+        self.url = ""
+        self.search_name = ""
+        self.min_price = 0
+        self.max_price = 0
+        self.filter_array = []
+        self.auto = Pasing(self.url, self.search_name, self.min_price, self.max_price, self.filter_array)
+
+    def swap_min_max_num(self):
+        if self.min_price > self.max_price:
+            self.min_price, self.max_price = self.max_price, self.min_price
+
+    def auto_method(self):
+        # 값을 불러와 저장.
+        self.url = entry_url.get() if entry_url.get() else "https://www.coupang.com"
+        self.search_name = entry_name.get() if entry_name.get() else "아이"
+        self.min_price = int(entry_min_price.get() if entry_min_price.get() else 0)
+        self.max_price = int(entry_max_price.get() if entry_max_price.get() else sys.maxsize)
+        self.filter_array = entry_filter.get() if entry_filter.get() else ""
+        self.filter_array = self.filter_array.split(",")
+        self.swap_min_max_num()
+        self.auto.set_member(self.url, self.search_name, self.min_price, self.max_price, self.filter_array)
+        #  매크로 실행
+        result = self.auto.auto_start()
+        # 매크로 실행 결과 출력
+        print(result)
+        if result == 'success':
+            result = self.save_to_csv()
+        print(result)
+
+    def save_to_csv(self):
+        return self.auto.ary_to_csv()
+
+
+def coupang_run():
+    # target에 auto.start()를하면 리턴값을 타겟에 지정하는 것이므로 주의 ()사용은 알고 쓰자.
+    thread = threading.Thread(target=macro.auto_method)
+    thread.daemon = True
+    thread.start()
+
+
+# 매크로 클래스 생성
+macro = Macro()
+
+window = tk.Tk()
+window.title("auto_Search")
+window.geometry("640x360+100+100")
+window.resizable(False, False)
+text_label = tk.LabelFrame(window, text='진행창', relief='solid', bd=1, pady=10)
+text_label.pack()
+text_label.place(x=5, y=10, width=320, height=350)
+
+button = tk.Button(window, anchor="center", text="자동화 시작", command=coupang_run, overrelief="solid",
+                   repeatdelay=1000)
+button.place(x=550, y=330)
+
+button = tk.Button(window, anchor="center", text="자동화 종료", command=lambda: macro.auto.thread_control(True),
+                   overrelief="solid",
+                   repeatdelay=1000)
+button.place(x=450, y=330)
+
+label_url = tk.Label(window, text="가격 필터")
+label_url.place(x=350, y=190)
+
+label_url = tk.Label(window, text="filter ','로 구분")
+label_url.place(x=350, y=220)
+
+label_url = tk.Label(window, text="url입력창")
+label_url.place(x=350, y=250)
+
+label_name = tk.Label(window, text="검색입력")
+label_name.place(x=350, y=280)
+
+label_name = tk.Label(window, text="~", font=("궁서체", 20))
+label_name.place(x=510, y=183)
+
+entry_min_price = tk.Entry(window, width=9)
+entry_min_price.place(x=440, y=190)
+
+entry_max_price = tk.Entry(window, width=11)
+entry_max_price.place(x=530, y=190)
+
+entry_filter = tk.Entry(window)
+entry_filter.place(x=450, y=220)
+
+entry_url = tk.Entry(window)
+entry_url.place(x=450, y=250)
+
+entry_name = tk.Entry(window)
+entry_name.place(x=450, y=280)
+
+window.mainloop()
